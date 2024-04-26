56426dd4df9d8246edf11c82f9c2a6a0bfb2147f
William Yang <dev.william.y@icarcover.com>
2024-04-26 10:39:54 -0700

    Added creation of relation product

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ async function fetchAndUploadData() {
     } = await supabase
       .from("reviews-2") // Replace 'source_table' with your source table name
       .select("*", { count: "exact" })
-      .order('id', { ascending: true })  // Ensure consistent ordering by a unique column
+      .order("id", { ascending: true }) // Ensure consistent ordering by a unique column
       .range(pageIndex * pageSize, (pageIndex + 1) * pageSize - 1);
 
     if (readError) {
@@ -58,4 +58,163 @@ async function fetchAndUploadData() {
   }
 }
 
-fetchAndUploadData();
+async function createRelationalTable() {
+  const pageSize = 4000;
+  const { data: models, error: modelError } = await supabaseAdmin
+    .from("model_20240425")
+    .select("id, name");
+  // Check if the make exists
+  const { data: makes, error: makeError } = await supabaseAdmin
+    .from("make_20240425")
+    .select("id, name");
+  const { data: types, error } = await supabaseAdmin
+    .from("type_20240425")
+    .select("id, name");
+
+  const { data: years, error: err_date } = await supabaseAdmin
+    .from("year_20240425")
+    .select("id, name");
+
+  let pageIndex = 0;
+  let hasMore = true;
+
+  while (hasMore) {
+    let {
+      data: products,
+      error,
+      count,
+    } = await supabaseAdmin
+      .from("Products_20240425")
+      .select("id, sku,make,model,year_generation,type")
+      .order("id", { ascending: true })
+      .range(pageIndex * pageSize, (pageIndex + 1) * pageSize - 1);
+    console.log(
+      `Retrieving Products...(${pageIndex * pageSize}, ${
+        (pageIndex + 1) * pageSize - 1
+      })`
+    );
+    if (error) {
+      console.error("Error fetching products:", error);
+      break; // Stop processing on error
+    }
+
+    // For each product
+    if (products.length === 0) {
+      console.log("Products.length is 0, setting has more to false");
+      hasMore = false;
+      break;
+    }
+
+    await prepareAndBatchInsertData(products, makes, models, years, types);
+    console.log(`Data inserted for batch ${pageIndex + 1}:`);
+
+
+    if (products.length < pageSize) {
+      console.log(
+        "Products.length is less than page size, setting has more to false"
+      );
+
+      hasMore = false; // No more pages left to fetch
+    } else {
+      pageIndex++; // Prepare for the next page
+    }
+    //   if (date?.id && type?.id && make?.id && model?.id) {
+    //     //   const { data: existingRelation, error: relationError } = await supabaseAdmin
+    //     //     .from("relations_product")
+    //     //     .select("id")
+    //     //     .eq("year_id", date.id)
+    //     //     .eq("model_id", model?.id)
+    //     //     .eq("make_id", make.id)
+    //     //     .eq("type_id", type.id)
+    //     //     .eq("product_id", item);
+
+    //     // If the combination already exists, log a message or handle it as needed
+    //     if (existingRelation && existingRelation.length > 0) {
+    //       console.log("Combination of IDs already exists in the database.");
+    //     } else {
+    //       // If the combination doesn't exist, insert the new data
+    //     }
+    //   }
+  }
+}
+
+async function prepareAndBatchInsertData(
+  products,
+  makes,
+  models,
+  years,
+  types
+) {
+  let inserts = [];
+  let insertCount = 0;
+  const batchSize = 1000;
+
+  for (const product of products) {
+    let row = {
+      product_id: 0,
+      type_id: 0,
+      year_id: 0,
+      make_id: 0,
+      model_id: 0,
+    };
+    row.product_id = product.id;
+    row.type_id = 1; // only have car cover now would need to look against type table too if this isn't valid
+
+    // Find make_id
+    const make = makes.find((m) => m.name === product.make);
+    if (make) {
+      row.make_id = make.id;
+    }
+
+    // Find model_id
+    const model = models.find((m) => m.name === product.model);
+    if (model) {
+      row.model_id = model.id;
+    }
+
+    const [startYear, endYear] = product.year_generation
+      .split("-")
+      .map((year) => parseInt(year));
+    if (!endYear) {
+      const yearObj = years.find((y) => y.name === startYear.toString());
+      row.year_id = yearObj.id;
+      inserts.push(row);
+    }
+    for (let year = startYear; year <= endYear; year++) {
+      const yearObj = years.find((y) => y.name === year.toString());
+      if (yearObj) {
+        row.year_id = yearObj.id;
+        // Assuming you might want a separate row for each year within the range
+        let newRow = { ...row, year_id: yearObj.id };
+        inserts.push(newRow);
+
+        // Check if the batch should be inserted
+        if (inserts.length >= batchSize) {
+          console.log(
+            `Batch size has been reached, inserting: ${inserts.length} records`
+          );
+          insertCount += inserts.length;
+          await batchInsert(inserts);
+          inserts = []; // Reset the batch after inserting
+        }
+      }
+    }
+  }
+
+  if (inserts.length > 0) {
+    console.log(`Found some leftover, inserting: ${inserts.length} records`);
+    insertCount += inserts.length;
+    await batchInsert(inserts);
+  }
+  console.log(`Reached end of program. Inserted ${insertCount} records`);
+}
+
+async function batchInsert(rows) {
+  const { data: relation_data, error: relationInsertError } =
+    await supabaseAdmin.from("relations_product_20240425").insert(rows);
+  if (relationInsertError) {
+    console.log(relationInsertError);
+  }
+}
+// fetchAndUploadData();
+createRelationalTable();
